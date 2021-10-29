83dbfc5c9c079e05a8feeba29f502baf72df817c
Horlamide <olamide.jubril@taxtech.com.ng>
2021-10-29 11:01:18 +0100

    Bug fixes

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const {
@@ -11,72 +11,69 @@ const {
   postReset,
   getNewPassword,
   postNewPassword,
-} = require('../controllers/auth');
-
-
+} = require("../controllers/auth");
 
 /***
  * @router  GET: /signup
  * @desc    Render view for signing up
  * @access  Public
  * ***/
-router.get('/signup', getSignUp);
+router.get("/signup", getSignUp);
 
 /***
  * @router  POST: /signup
  * @desc    Signup user
  * @access  Public
  * ***/
-router.post('/signup', postSignUp);
+router.post("/signup", postSignUp);
 
 /***
  * @router  GET: /login
  * @desc    Render view for login
  * @access  Public
  * ***/
-router.get('/login', getLogin);
+router.get("/login", getLogin);
 
 /***
  * @router  POST: /login
  * @desc    Authenticate user/login user
  * @access  Public
  * ***/
-router.post('/login', postLogin);
+router.post("/login", postLogin);
 
 /***
  * @router  POST: /logout
  * @desc    Authenticate user/logout user
  * @access  Public
  * ***/
-router.post('/logout', postLogout);
+router.post("/logout", postLogout);
 
 /***
  * @router  GET: /pass-reset
  * @desc    Render view for password reset
  * @access  Public
  * ***/
-router.get('/pass-reset', getReset);
+router.get("/pass-reset", getReset);
 
 /***
  * @router  POST: /pass-reset
  * @desc    Reset password
  * @access  Public
  * ***/
-router.post('/pass-reset', postReset);
+router.post("/pass-reset", postReset);
 
 /***
  * @router  GET: /new-password
  * @desc    Render view for new password
  * @access  Public
  * ***/
-router.get('/new-password/:token', getNewPassword);
-
+router.get("/new-password/:token", getNewPassword);
 
 /***
  * @router  POST: /new-password
  * @desc    Render view for new password
  * @access  Public
  * ***/
-router.post('/new-password', postNewPassword);
+router.post("/new-password", postNewPassword);
 
 module.exports = router;
